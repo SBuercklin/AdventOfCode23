05e81e4c5c34af0af70981bbc5e199190618549e
Sam Buercklin <sam.buercklin@gmail.com>
2023-12-19 11:05:20 -0500

    finally day 18

--- src/day18/mod.rs
+++ src/day18/mod.rs
@@ -11,7 +11,7 @@ use hex;
 use nom::InputIter;
 
 type BOARD = AoCMatrix<Square>;
-type POSITION = (usize, usize);
+type POSITION = (i64, i64);
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 enum Square {
@@ -65,7 +65,7 @@ impl Color {
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 struct Command {
     direction: char,
-    distance: usize,
+    distance: i64,
     color: Color,
 }
 
@@ -114,125 +114,73 @@ struct HorizontalLine {
 */
 pub fn part1(lines: Vec<String>) -> i64 {
     let cmds: Vec<Command> = lines.iter().map(parse_line).collect();
-    return solve(cmds);
+    return solve(&cmds);
 }
 
 pub fn part2(lines: Vec<String>) -> i64 {
-    // let cmds = lines.iter().map(parse_line_color_command).collect();
-    return 1;
+    let cmds = lines.iter().map(parse_line_color_command).collect();
+    return solve(&cmds);
 }
 
 /*
-    Working with lines
+    Let's try shoelace again
 */
 
-fn solve(cmds: Vec<Command>) -> i64 {
-    let (horz, mut verts) = cmds_to_lines(cmds);
-    let maxy = verts.iter().map(|v| v.top).max().unwrap();
-    let miny = verts.iter().map(|v| v.bot).min().unwrap();
+fn solve(cmds: &Vec<Command>) -> i64 {
+    let (pts, l) = pts_from_cmds(cmds);
+    let area = shoelace(pts);
 
-    let mut adjacency = HashSet::new();
-    adjacency.insert((*verts.first().unwrap(), *verts.last().unwrap()));
-    adjacency.insert((*verts.last().unwrap(), *verts.first().unwrap()));
-    for i in 0..(verts.len() - 1) {
-        let a = verts[i];
-        let b = verts[i + 1];
-        adjacency.insert((a, b));
-        adjacency.insert((b, a));
-    }
-
-    verts.sort_by(|a, b| a.x.cmp(&b.x));
-
-    let mut acc = horz.iter().map(|h| h.length).sum();
-
-    for row in miny..=maxy {
-        let mut cacc = 0;
-        let mut next_step_counts = true; // We start outside
-        let mut prev_step_counted = false;
-        let lverts: Vec<&VerticalLine> = verts.iter().filter(|v| v.intersects(row)).collect();
-        for i in 0..lverts.len() {
-            if i == lverts.len() - 1 {
-                cacc += 1
-            } else {
-                let a = lverts[i];
-                let b = lverts[i + 1];
-                let adjacent = adjacency.get(&(*a, *b)).is_some();
-
-                // By default, count the first
-                cacc += 1;
-
-                if adjacent
-                    && ((a.top == row && a.top == b.top) || (a.bot == row && a.bot == b.bot))
-                {
-                    next_step_counts = !next_step_counts;
-                } else if adjacent
-                    && ((a.top == row && a.top == b.bot) || (a.bot == row && a.bot == b.top))
-                {
-                } else {
-                    if next_step_counts {
-                        cacc += b.x - a.x - 1;
-                        next_step_counts = false;
-                    } else {
-                        next_step_counts = true;
-                    }
-                }
-            }
-        }
-        acc += cacc;
-    }
-
-    return acc;
+    // UNDERSTANDING EACH TERM:
+    // Assume that shoelace traces out the **bottom right** vertex of each square.
+    //      This means that any square which is ABOVE or LEFT of the current movement
+    //      is not INSIDE the border being traced, but anything BELOW or RIGHT of the
+    //      current movement is.
+    //
+    //      Since half of our movement has to be DOWN and RIGHT, and half has to be UP
+    //      and LEFT, we only need to add HALF of the length, since the shoelace catches
+    //      the other half.
+    //
+    //      Plus 1 accounts for first square
+    return area + l / 2 + 1;
 }
 
-fn cmds_to_lines(cmds: Vec<Command>) -> (Vec<HorizontalLine>, Vec<VerticalLine>) {
-    let mut cpos = (0 as i64, 0 as i64);
-    let mut verts = vec![];
-    let mut horiz = vec![];
-
-    for i in 0..cmds.len() {
-        let c = cmds[i];
-        let next_cmd = cmds[i + 1 % cmds.len()];
-        let prev_cmd = cmds[(i + cmds.len() - 1) % cmds.len()];
-        let old_pos = cpos.clone();
-        let dist = c.distance as i64;
-        match c.direction {
-            'U' => {
-                cpos.1 += dist;
-                if dist > 1 {
-                    verts.push(VerticalLine {
-                        top: cpos.1,
-                        bot: old_pos.1,
-                        x: cpos.0,
-                        bot_dir: Direction::from_char(prev_cmd.direction),
-                        top_dir: Direction::from_char(next_cmd.direction),
-                    })
-                }
-            }
-            'D' => {
-                cpos.1 -= dist;
-                if dist > 1 {
-                    verts.push(VerticalLine {
-                        top: old_pos.1,
-                        bot: cpos.1,
-                        x: cpos.0,
-                        bot_dir: Direction::from_char(next_cmd.direction),
-                        top_dir: Direction::from_char(prev_cmd.direction),
-                    })
-                }
-            }
-            'L' => {
-                cpos.0 -= dist;
-                horiz.push(HorizontalLine { length: dist - 1 })
-            }
-            'R' => {
-                cpos.0 += dist;
-                horiz.push(HorizontalLine { length: dist - 1 })
-            }
-            _ => (),
-        }
+fn pts_from_cmds(cmds: &Vec<Command>) -> (Vec<POSITION>, i64) {
+    let mut cpos = (0, 0);
+    let mut pvec = vec![cpos];
+    let mut l_acc = 0;
+    for c in cmds.iter() {
+        let d = c.distance;
+        l_acc += d;
+        cpos = match c.direction {
+            'U' => (cpos.0, cpos.1 + d),
+            'D' => (cpos.0, cpos.1 - d),
+            'L' => (cpos.0 - d, cpos.1),
+            'R' => (cpos.0 + d, cpos.1),
+            _ => panic!(),
+        };
+        pvec.push(cpos);
     }
 
-    return (horiz, verts);
+    return (pvec, l_acc);
+}
+
+fn shoelace(pts: Vec<POSITION>) -> i64 {
+    let mut area_acc = 0;
+    for i in 0..pts.len() - 1 {
+        let a = pts[i];
+        let b = pts[i + 1];
+        area_acc += cross_det(a, b);
+    }
+
+    return area_acc.abs() / 2;
+}
+
+fn cross_det(a: POSITION, b: POSITION) -> i64 {
+    a.0 * b.1 - b.0 * a.1
+}
+
+fn length(a: POSITION, b: POSITION) -> i64 {
+    return (b.0 - a.0).abs() + (a.1 - b.1).abs();
 }
 
 /*
@@ -242,7 +190,7 @@ fn cmds_to_lines(cmds: Vec<Command>) -> (Vec<HorizontalLine>, Vec<VerticalLine>)
 fn parse_line(l: &String) -> Command {
     let mut pieces = l.split(' ');
     let dir = pieces.next().unwrap().chars().next().unwrap();
-    let count: usize = pieces.next().unwrap().parse().unwrap();
+    let count: i64 = pieces.next().unwrap().parse().unwrap();
 
     let color = Color::from_hex(&pieces.next().unwrap()[1..9]);
 
@@ -269,7 +217,7 @@ fn parse_line_color_command(l: &String) -> Command {
         _ => panic!("Unrecognized direction"),
     };
 
-    let count = usize::from_str_radix(&color_str[2..7], 16).unwrap();
+    let count = i64::from_str_radix(&color_str[2..7], 16).unwrap();
 
     return Command {
         direction: dir,
@@ -298,22 +246,12 @@ mod tests {
     }
 
     #[test]
-    fn part1_test_by_hand() {
-        let string_input = "R 9 (#70c710)\nD 3 (#0dc571)\nL 3 (#5713f0)\nD 3 (#d2c081)\nL 3 (#59c680)\nU 3 (#411b91)\nL 5 (#8ceee2)\nU 2 (#caa173)\nL 1 (#1b58a2)\nU 2 (#caa171)\nR 2 (#7807d2)\nU 3 (#a77fa3)\nL 2 (#015232)\nU 2 (#7a21e3)".to_string();
+    fn part2_test() {
+        let string_input = "R 6 (#70c710)\nD 5 (#0dc571)\nL 2 (#5713f0)\nD 2 (#d2c081)\nR 2 (#59c680)\nD 2 (#411b91)\nL 5 (#8ceee2)\nU 2 (#caa173)\nL 1 (#1b58a2)\nU 2 (#caa171)\nR 2 (#7807d2)\nU 3 (#a77fa3)\nL 2 (#015232)\nU 2 (#7a21e3)".to_string();
         let line_input = string_to_lines(&string_input);
 
-        let result = part1(line_input);
+        let result = part2(line_input);
 
-        assert_eq!(result, 62);
+        assert_eq!(result, 952408144115);
     }
-
-    // #[test]
-    // fn part2_test() {
-    //     let string_input = "R 6 (#70c710)\nD 5 (#0dc571)\nL 2 (#5713f0)\nD 2 (#d2c081)\nR 2 (#59c680)\nD 2 (#411b91)\nL 5 (#8ceee2)\nU 2 (#caa173)\nL 1 (#1b58a2)\nU 2 (#caa171)\nR 2 (#7807d2)\nU 3 (#a77fa3)\nL 2 (#015232)\nU 2 (#7a21e3)".to_string();
-    //     let line_input = string_to_lines(&string_input);
-
-    //     let result = part2(line_input);
-
-    //     assert_eq!(result, 952408144115);
-    // }
 }
